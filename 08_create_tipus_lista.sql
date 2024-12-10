9fe7cbf8dcc8b5ac2b6fd5d7ad8c4917ba1a1120
Kristof Novak <114155561+knov1115@users.noreply.github.com>
2024-12-10 12:29:52 +0100

    Update 08_create_tipus_lista.sql
    WDQ

--- 08_create_tipus_lista.sql
+++ 08_create_tipus_lista.sql
@@ -1,5 +1,6 @@
 CREATE OR REPLACE TYPE ty_kereskedes_statisztika_l AS TABLE OF ty_kereskedes_statisztika;
 
+--ELLENŐRZÉS
 SELECT type_name, type_oid
 FROM user_types
 WHERE type_name IN ('TY_KERESKEDES_STATISZTIKA', 'TY_KERESKEDES_STATISZTIKA_L');
